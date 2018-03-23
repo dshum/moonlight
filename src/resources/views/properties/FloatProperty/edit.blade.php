b77a4590447ac45a287a882dbd18ffedef7bce39
Denis Shumeev <denis-shumeev@yandex.ru>
2018-03-23 20:21:17 +0300

    Properties.

--- src/resources/views/properties/FloatProperty/edit.blade.php
+++ src/resources/views/properties/FloatProperty/edit.blade.php
@@ -1,4 +1,4 @@
-<label>{{ $title }}:</label><br>
+<label>{{ $title }}:</label><span name="{{ $name }}" class="error"></span><br>
 @if ($readonly)
 <input type="text" name="{{ $name }}" value="{{ $value }}" class="number" placeholder="{{ $title }}" readonly>
 @else
