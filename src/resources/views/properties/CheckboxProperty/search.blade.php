8061f217606005e1ff404f8311ceceb00f41f062
Denis Shumeev <denis-shumeev@yandex.ru>
2018-12-26 16:49:14 +0300

    Fix missing name.

--- src/resources/views/properties/CheckboxProperty/search.blade.php
+++ src/resources/views/properties/CheckboxProperty/search.blade.php
@@ -1,6 +1,6 @@
 <div class="label checkbox"><i class="fa fa-check-square"></i><span>{{ $title }}</span></div>
 <div>
-    <select>
+    <select name="{{ $name }}">
         <option value="">Не важно</option>
         <option value="true"{{ $value === 'true' ? ' selected' : '' }}>Да</option>
         <option value="false"{{ $value === 'false' ? ' selected' : '' }}>Нет</option>
