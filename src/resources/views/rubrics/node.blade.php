009d2641d9dfb006c3b5592525430fc5f1ad687e
Denis Shumeev <denis-shumeev@yandex.ru>
2019-04-04 15:15:58 +0300

    CSS.

--- src/resources/views/rubrics/node.blade.php
+++ src/resources/views/rubrics/node.blade.php
@@ -2,10 +2,10 @@
     @foreach ($elements as $element)
     <li class="{{ $element['classId'] == $classId ? 'active' : '' }}">
         @if (isset($children[$element['classId']]))
-        <a href="{{ route('moonlight.browse.element', $element['classId']) }}" item="{{ $element['itemName'] }}">{{ $element['name'] }}</a><span class="open" rubric="{{ $name }}" bind="{{ $bind }}" classId="{{ $element['classId'] }}" display="show"><i class="fa fa-angle-down"></i></span>
+        <span class="wrap"><a href="{{ route('moonlight.browse.element', $element['classId']) }}" item="{{ $element['itemName'] }}">{{ $element['name'] }}</a>&nbsp;<span class="open" rubric="{{ $name }}" bind="{{ $bind }}" classId="{{ $element['classId'] }}" display="show"><i class="fa fa-angle-down"></i></span></span>
         {!! $children[$element['classId']] !!}
         @elseif (isset($haschildren[$element['classId']]))
-        <a href="{{ route('moonlight.browse.element', $element['classId']) }}" item="{{ $element['itemName'] }}">{{ $element['name'] }}</a><span class="open" rubric="{{ $name }}" bind="{{ $bind }}" classId="{{ $element['classId'] }}" display="none"><i class="fa fa-angle-down"></i></span>
+        <span class="wrap"><a href="{{ route('moonlight.browse.element', $element['classId']) }}" item="{{ $element['itemName'] }}">{{ $element['name'] }}</a>&nbsp;<span class="open" rubric="{{ $name }}" bind="{{ $bind }}" classId="{{ $element['classId'] }}" display="none"><i class="fa fa-angle-down"></i></span></span>
         @else
         <a href="{{ route('moonlight.browse.element', $element['classId']) }}" item="{{ $element['itemName'] }}">{{ $element['name'] }}</a>
         @endif
