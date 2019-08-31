97d1ef743b5d40ff4dda45fed7a87a3e19291271
Denis Shumeev <denis-shumeev@yandex.ru>
2019-08-31 12:40:33 +0300

    CSRF added.

--- src/resources/views/layouts/base.blade.php
+++ src/resources/views/layouts/base.blade.php
@@ -1,35 +1,42 @@
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0">
-<meta name="msapplication-tap-highlight" content="no">
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<title>@yield('title')</title>
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/font-awesome.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/default.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/loader.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/calendar.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/autocomplete.min.css">
-@section('css')
-@show
-@if (isset($styles))
-@foreach ($styles as $style)
-<link media="all" type="text/css" rel="stylesheet" href="{{ $style }}">
-@endforeach
-@endif
-<script src="/packages/moonlight/js/jquery/jquery-3.2.1.min.js"></script>
-<script src="/packages/moonlight/js/jquery/jquery.form.min.js"></script>
-<script src="/packages/moonlight/js/autocomplete/jquery.autocomplete.min.js"></script>
-<script src="/packages/moonlight/js/sortable/sortable.min.js"></script>
-<script src="/packages/moonlight/js/calendar/jquery.calendar.min.js"></script>
-<script src="/packages/moonlight/js/common.min.js"></script>
-@section('js')
-@show
-@if (isset($scripts))
-@foreach ($scripts as $script)
-<script src="{{ $script }}"></script>
-@endforeach
-@endif
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/font-awesome.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/default.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/loader.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/calendar.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/autocomplete.min.css">
+    @section('css')
+    @show
+    @if (isset($styles))
+        @foreach ($styles as $style)
+            <link media="all" type="text/css" rel="stylesheet" href="{{ $style }}">
+        @endforeach
+    @endif
+    <script src="/packages/moonlight/js/jquery/jquery-3.2.1.min.js"></script>
+    <script src="/packages/moonlight/js/jquery/jquery.form.min.js"></script>
+    <script src="/packages/moonlight/js/autocomplete/jquery.autocomplete.min.js"></script>
+    <script src="/packages/moonlight/js/sortable/sortable.min.js"></script>
+    <script src="/packages/moonlight/js/calendar/jquery.calendar.min.js"></script>
+    <script src="/packages/moonlight/js/common.min.js"></script>
+    @section('js')
+    @show
+    @if (isset($scripts))
+        @foreach ($scripts as $script)
+            <script src="{{ $script }}"></script>
+        @endforeach
+    @endif
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 </head>
 <body>
 @section('nav')
@@ -38,21 +45,21 @@
 @show
 @section('body')
 @show
-    <div class="sidebar-block-ui"></div>
-    <div class="block-ui">
-        <div class="container">
-            <div class="wrapper">
-                <div class="cssload-loader"></div>
-            </div>
-        </div>
-    </div>
-    <div class="alert">
+<div class="sidebar-block-ui"></div>
+<div class="block-ui">
+    <div class="container">
         <div class="wrapper">
-            <div class="container">
-                <div class="hide">&#215;</div>
-                <div class="content"></div>
-            </div>
+            <div class="cssload-loader"></div>
         </div>
     </div>
+</div>
+<div class="alert">
+    <div class="wrapper">
+        <div class="container">
+            <div class="hide">&#215;</div>
+            <div class="content"></div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- src/resources/views/layouts/login.blade.php
+++ src/resources/views/layouts/login.blade.php
@@ -1,34 +1,43 @@
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0">
-<meta name="msapplication-tap-highlight" content="no">
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<title>@yield('title')</title>
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/font-awesome.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/default.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/loader.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/login.min.css">
-@section('css')
-@show
-<style>
-body {background-image: url(/assets/background.jpg?{{ date('Y-m-d-H-00') }});}
-</style>
-<script src="/packages/moonlight/js/jquery/jquery-3.2.1.min.js"></script>
-<script src="/packages/moonlight/js/jquery/jquery.form.min.js"></script>
-<script src="/packages/moonlight/js/common.min.js"></script>
-@section('js')
-@show
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/font-awesome.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/default.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/loader.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/moonlight/css/login.min.css">
+    @section('css')
+    @show
+    <style>
+        body {
+            background-image: url(/assets/background.jpg?{{ date('Y-m-d-H-00') }});
+        }
+    </style>
+    <script src="/packages/moonlight/js/jquery/jquery-3.2.1.min.js"></script>
+    <script src="/packages/moonlight/js/jquery/jquery.form.min.js"></script>
+    <script src="/packages/moonlight/js/common.min.js"></script>
+    @section('js')
+    @show
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 </head>
 <body>
 @section('content')
 @show
-    <div class="block-ui">
-        <div class="container">
-            <div class="wrapper">
-                <div class="cssload-loader"></div>
-            </div>
+<div class="block-ui">
+    <div class="container">
+        <div class="wrapper">
+            <div class="cssload-loader"></div>
         </div>
     </div>
+</div>
 </body>
 </html>
