c84ce4eb2a6da7977dd8d010d51b818080189e10
Denis Shumeev <denis-shumeev@yandex.ru>
2021-11-27 19:56:38 +0300

    Added attribute autocomplete="suggest-password" due to the Chrome requirements.

--- src/resources/views/properties/PasswordProperty/edit.blade.php
+++ src/resources/views/properties/PasswordProperty/edit.blade.php
@@ -1,6 +1,6 @@
 <label>{{ $title }}:</label><br>
 @if ($readonly)
-    <input type="password" name="{{ $name }}" placeholder="{{ $title }}" readonly>
+    <input type="password" name="{{ $name }}" placeholder="{{ $title }}" autocomplete="suggest-password" readonly>
 @else
-    <input type="password" name="{{ $name }}" placeholder="{{ $title }}">
+    <input type="password" name="{{ $name }}" placeholder="{{ $title }}" autocomplete="suggest-password">
 @endif
